03261a0c155f6877c9cd8e51688b7b6dc6561212
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-11-14 12:42:53 +0100

    Sync

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Scool\EbreEscoolModel\Providers\EbreEscoolMigratorService;
+
 return [
 
     /*
@@ -166,6 +168,7 @@ return [
         /*
          * Package Service Providers...
          */
+        EbreEscoolMigratorService::class,
 
         //
 
